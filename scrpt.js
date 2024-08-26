8b36092f6b7ae35fdf70ba53d45c8991e2d61d25
covenantofzionmountain <zionmountainministry@hotmail.com>
2024-08-27 00:11:42 +0100

    simple popup

---
+++ scrpt.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+//open the popup
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+//close the popup
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
